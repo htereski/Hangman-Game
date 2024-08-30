51ebf467c699828927161f17c46242f9f921e62c
htereski <tereski84@gmail.com>
2024-08-30 15:41:04 -0300

    arrumando updateWithImg

--- app/Repositories/CategoryRepository.php
+++ app/Repositories/CategoryRepository.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace App\Repositories;
 
@@ -6,17 +6,23 @@ use App\Models\Category;
 use Exception;
 use Illuminate\Http\Request;
 
-class CategoryRepository extends Repository { 
+class CategoryRepository extends Repository
+{
 
     protected $rows = 6;
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct(new Category());
-    }   
+    }
 
-    public function getRows() { return $this->rows; }
+    public function getRows()
+    {
+        return $this->rows;
+    }
 
-    public function saveWithImg(Request $request) {
+    public function saveWithImg(Request $request)
+    {
         $category = new Category();
         $category->name = $request->name;
         $category->url = 'undefined';
@@ -31,18 +37,21 @@ class CategoryRepository extends Repository {
         return $this->save($category);
     }
 
-    public function updateWithImg(Request $request, string $id) {
+    public function updateWithImg(Request $request, string $id)
+    {
         $category = $this->findById($id);
 
         if (isset($category)) {
-            $category->name = $request->name;
+            if ($request->hasFile('img')) {
+                $category->name = $request->name;
 
-            $ext = $request->file('img')->getClientOriginalExtension();
-            $nome_arq = $category->id . "_" . time() . "." . $ext;
-            $request->file('img')->storeAs("public/", $nome_arq);
+                $ext = $request->file('img')->getClientOriginalExtension();
+                $nome_arq = $category->id . "_" . time() . "." . $ext;
+                $request->file('img')->storeAs("public/", $nome_arq);
+                $category->url = "/storage/" . $nome_arq;
 
-            $category->url =  $nome_arq;
-            return $this->save($category);
+                return $this->save($category);
+            }
         }
         return false;
     }
